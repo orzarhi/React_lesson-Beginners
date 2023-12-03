b1cfbcec4feb03ad33679d88c8e6451fb0146e63
Maxsoft Ltd <email@example.com>
2023-12-03 17:16:39 +0200

    Update About.jsx and Todo.css files

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export const About = () => {
         <div className='container'>
             <h1 className='title'>Hi, my name is Or</h1>
             <span className='description'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, in pariatur error eum temporibus alias placeat hic nostrum accusantium, reprehenderit, eligendi quidem nulla omnis facere quo quas? Ipsum, officia laborum.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, obcaecati. Excepturi delectus non officia? Excepturi aliquid et minus, sit dignissimos esse provident? Deserunt facilis optio nobis quos, iusto officia ipsam.
             </span>
         </div>
     )
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,2 @@
 export const wait = async (time) => new Promise((resolve) => setTimeout(resolve, time))
 export const reject = async (time) => new Promise((_, reject) => setTimeout(reject, time))
-
